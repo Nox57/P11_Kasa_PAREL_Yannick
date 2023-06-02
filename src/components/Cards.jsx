331836d347ce7529a7b4470c2d708d3ac5ab6e6b
Nox57 <yannickparel@gmail.com>
2023-06-02 19:58:39 +0200

    added card for each accommodation in homepage (cards.jsx component)

---
+++ src/components/Cards.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import jsonData from '../datas/logements.json'
+import '../styles/Cards.css'
+
+function Cards() {
+    const [cardsData, setCardData] = useState([])
+
+    useEffect(() => {
+        setCardData(jsonData)
+    }, [])
+
+    return (
+        <div className="cards-container">
+            {cardsData.map((Card) => (
+                <div
+                    className="card"
+                    key={Card.id}
+                    style={{
+                        backgroundImage:
+                            'linear-gradient(180deg, rgba(255, 255, 255, 0) 70%, rgba(0, 0, 0, 0.5) 100%), url(' +
+                            Card.cover +
+                            ')',
+                        backgroundSize: 'cover',
+                    }}
+                >
+                    <Link to="/">
+                        <h2>{Card.title}</h2>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Cards
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from '../components/Banner'
+import Cards from '../components/Cards'
 import '../styles/Home.css'
 
 function Home() {
     return (
         <div className="home">
             <Banner />
+            <Cards />
         </div>
     )
 }
